b3e393d3592ac79ab2b69dbee3d6b9b10c242753
Cinque <26643549@qq.com>
2023-05-27 06:57:56 +0800

    Add files via upload

---
+++ ItemIndexConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfApp4
+{
+    internal class ItemIndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DependencyObject obj)
+            {
+                ItemsControl itemsControl = ItemsControl.ItemsControlFromItemContainer(obj);
+                int index = itemsControl.ItemContainerGenerator.IndexFromContainer(obj);
+                return index + 1;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
